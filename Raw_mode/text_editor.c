90a4feffea04c5aa0f83be91e11c125943ff3575
Varad Bendale <varadbendale17@gmail.com>
2026-05-27 23:10:30 +0530

    Add files via upload

--- Raw_mode/text_editor.c
+++ Raw_mode/text_editor.c
@@ -6,16 +6,16 @@
 #include<errno.h>
 struct termios original  ; 
 
-void disable_raw_mode(){
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, & original) == -1){
-  	  die("tcsetattr");
-	}
-}
-
 void die(const char *s) { 
    perror( s ) ; 
    exit(1) ; 
 } 
+
+void disable_raw_mode(){
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original) == -1){
+  	  die("tcsetattr");
+	}
+}
 
 void rawmode(){
     if (tcgetattr(STDIN_FILENO, &original) == -1){
@@ -25,13 +25,12 @@ void rawmode(){
     temp.c_iflag &= ~(IXON | ICRNL ) ; 
     temp.c_oflag &= ~(OPOST ) ; 
     temp.c_lflag &= ~(IEXTEN|ECHO|ICANON|ISIG ) ; 
-    temp.c_cc[VMIN] = 0
-    temp.c_cc[VTIME] = 1
-    temp.c_oflag &= ~(OPOST ) ; 
-    if (tcsetattr(STDIN_FILENO, &original) == -1){
+    temp.c_cc[VMIN] = 0 ; 
+    temp.c_cc[VTIME] = 1 ; 
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH , &temp) == -1){
        die("tcsetattr");
     } 
-    if (atexit(disable_raw_mode) == -1){
+    if (atexit(disable_raw_mode) != 0 ){
        die("atexit");
     } 
 }
@@ -39,7 +38,7 @@ void rawmode(){
 int main(){
     rawmode() ; 
     while(1) {
-    char c = '\0';
+    char c = '\0' ; 
     if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
        die("read");
     } 
